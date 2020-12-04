94b271cc6eb18a7953e7de1806a3d8b4f3abade1
mattieuf <ffmattieu@gmail.com>
2020-12-04 16:29:36 +0100

    petits rajouts

--- fonctions_projet.py
+++ fonctions_projet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import math
-import sys, re, os
+import sys, re, os, random, subprocess
 
 
 def test_premier(n):
@@ -13,10 +13,64 @@ def test_premier(n):
 		return False
 	if n%5 == 0:
 		return False
+	#on enlève les cas du début 
 	k = int(math.sqrt(n)) + 1
 	for i in range(7,k):
 		if n %k == 0:
 			return False
 	return True
 
-print(test_premier(341))
+def pgcd(a, b):
+	x,y, u,v=0,1, 1,0
+	while a != 0:
+		q, r = b//a, b%a
+		m, n = x-u*q, y-v*q
+		b,a, x,y, u,v= a,r, u,v, m,n
+	gcd = b
+	return gcd, x, y
+
+def nombre_premier(n):
+	n_0=random.choice('1379')
+	n_max=random.choice('123456789')
+
+	n_i=n_0
+
+	for i in range(1,n-1):
+		n_i=n_i+random.choice('0123456789')
+	
+	n_i=n_i+n_max
+	p=int(n_i)
+
+	commande=subprocess.run("openssl prime %d 2> /dev/stdout"%p, shell=True, stdout=subprocess.PIPE)
+	mon_exp_reguliere=re.compile(r"is not prime")
+
+	resultat=mon_exp_reguliere.search(str(commande))
+
+	while(resultat):
+		n_i=n_i[1:]+random.choice('0123456789')+random.choice('1379')
+		p=int(n_i)
+		commande=subprocess.run("openssl prime %d 2> /dev/stdout"%p, shell=True, stdout=subprocess.PIPE)
+		resultat=mon_exp_reguliere.search(str(commande))
+	return p
+
+
+def modinv(a, m):
+	gcd, x, y = pgcd(a, m)
+	if gcd != 1:
+		return None
+	return x % m
+
+def decoupage(message, l):
+	#fonctions pour découper un message en morceaux de longueur l
+	#problème si len(message) n'est pas multiple de l
+	n = len(message)
+	if n%l !=0:
+		message = message + (l - n%l)*"X" #padding avec des X
+	n = len(message)
+	Liste_message = []
+	a = 0
+	b = l
+	q = n//l
+	for i in range(q):
+		Liste_message.append(message[a+i*l : l+i*l])
+	return Liste_message
