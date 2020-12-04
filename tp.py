aeab172167f2627c7c90bc1d45b52cc663dc62c3
Remi <r.dupont87@outlook.fr>
2020-12-04 15:55:45 +0100

    test

--- tp.py
+++ tp.py
@@ -6,28 +6,52 @@ import random
 import re
 
 random.seed()
-n_0=random.choice('1379')
-n_max=random.choice('123456789')
 
-n_i=n_0
-max=3
+def nombre_premier(n):
+	n_0=random.choice('1379')
+	n_max=random.choice('123456789')
 
-for i in range(1,max-1):
-	n_i=n_i+random.choice('0123456789')
+	n_i=n_0
+
+	for i in range(1,n-1):
+		n_i=n_i+random.choice('0123456789')
 	
-n_i=n_i+n_max
-p=int(n_i)
-
-commande=subprocess.run("openssl prime %d 2> /dev/stdout"%p, shell=True, stdout=subprocess.PIPE)
-mon_exp_reguliere=re.compile(r"is not prime")
-
-resultat=mon_exp_reguliere.search(str(commande))
-
-while(resultat):
-	n_i=n_i[1:]+random.choice('1379')
+	n_i=n_i+n_max
 	p=int(n_i)
+
 	commande=subprocess.run("openssl prime %d 2> /dev/stdout"%p, shell=True, stdout=subprocess.PIPE)
+	mon_exp_reguliere=re.compile(r"is not prime")
+
 	resultat=mon_exp_reguliere.search(str(commande))
-else:
-	print("c'est un premier")
-	print(n_i)
+
+	while(resultat):
+		n_i=n_i[1:]+random.choice('0123456789')+random.choice('1379')
+		p=int(n_i)
+		commande=subprocess.run("openssl prime %d 2> /dev/stdout"%p, shell=True, stdout=subprocess.PIPE)
+		resultat=mon_exp_reguliere.search(str(commande))
+	return p
+	
+def pgcd(a, b):
+	x,y, u,v=0,1, 1,0
+	while a != 0:
+		q, r = b//a, b%a
+		m, n = x-u*q, y-v*q
+		b,a, x,y, u,v= a,r, u,v, m,n
+	gcd = b
+	return gcd, x, y
+	
+def modinv(a, m):
+	gcd, x, y = pgcd(a, m)
+	if gcd != 1:
+		return None
+	return x % m
+
+
+p=nombre_premier(5)
+q=nombre_premier(8)
+print(p)
+print(q)
+
+n=p*q
+phi_n=(p-1)*(q-1)
+
