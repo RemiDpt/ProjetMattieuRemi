182af6b492bb258c44c7423260ad2090962e2eda
mattieuf <ffmattieu@gmail.com>
2021-01-05 22:18:04 +0100

    nouvelles fonctions

--- tp.py
+++ tp.py
@@ -6,35 +6,54 @@ import random
 import re
 from fonctions_projet import *
 
-random.seed()
-
-#On choisit p et q premiers pour calculer n
-#on a de base e = 65 537
-#on recherche inverse de e mod (p-1)(q-1)
 e = 65537
-p=nombre_premier(5)
-q=nombre_premier(8)
-print(p)
-print(q)
-
-n=p*q
-phi_n=(p-1)*(q-1)
-
-#Alice envoie n et e à Bob
-cle_pub = (e, n)
-cle_priv = (e , d)
-
-
-M = 55551
-#Bob utilise e pour chiffrer M
-
-C = powmod(M,e,n)
-#Bob envoie C à Alice
-
-#Alice calcule l'inverse de e mod phi_n
+p = nombre_premier(2)
+q = nombre_premier(2)
+n = p * q
+phi_n = (p-1) * (q-1)
 d = modinv(e, phi_n)
 
-#Alice peut déchiffrer le message de Bob
-DM = powmod(C,d,n)
+def nouv_chif(chaine,e,n):
+	S = []
+	A = bytes(chaine,'utf-8')
+	for u in A:
+		S.append(u)
+	for i in range(len(S)):
+		if (S[i]<100):
+			S[i] +=900
+		S[i]=str(S[i])
+	print(S)
+	X = ''.join(S)
+	print(X)
+	X = bytes(str(powmod(int(X),e,n)),'utf-8')
+	return X
 
+def nouv_dech(bitbit,d,n):
+	X = int(bitbit.decode('utf-8'))
+	X = str(powmod(X,d,n))
+	print(X)
+	M = decoupage(X,3)
+	print(M)
+	for i in range(len(M)):
+		M[i] = int(M[i])
+		if (M[i]>=900):
+			M[i] -= 900
+	MesFin = (bytes(M)).decode('utf-8')
+	return MesFin
 
+message = "mon cochon marche au plafond"
+PO = decoupage(message,3)
+
+P = bytes(message,'utf-8')
+O = []
+for u in P:
+		O.append(u)
+
+NPO = []
+for i in range(len(PO)):
+	NPO.append(nouv_chif(PO[i],e,n))
+
+Z = []
+for j in range(len(NPO)):
+	Z.append(nouv_dech(NPO[j],d,n))
+print("Z = : ",Z)
